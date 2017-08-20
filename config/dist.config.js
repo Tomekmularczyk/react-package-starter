202e9ac7eecbd5a5c5b23d4a527fdaf1c9af31f8
TomekMularczyk <tomasz.mularczyk@risendot.com>
2017-08-20 12:56:52 +0200

    - initial

---
+++ config/dist.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+
+  entry: {
+    index: './src/HelloWorld.jsx',
+  },
+
+  output: {
+    path: path.resolve(__dirname, '../lib'),
+    filename: '[name].js',
+    library: 'react-component-test',
+    libraryTarget: 'umd',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [
+      "node_modules",
+      "../src",
+    ],
+    extensions: ['.js', '.jsx'],
+  },
+
+  externals: [
+    "react", "react-dom"
+  ],
+};
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelloWorld = () => (
+  <div className="HelloWorld">
+    HelloWorld Component, rozumiecie to?
+  </div>
+);
+
+export default HelloWorld;
